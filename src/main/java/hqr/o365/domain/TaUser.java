7a44ec77e463237f4e6bfa2e2a66965ec60ef21d
He Rui Long <ruilong@ncsi.com.cn>
2021-04-23 10:58:46 +0800

    build 1

---
+++ src/main/java/hqr/o365/domain/TaUser.java
@@ -0,0 +1,33 @@
+package hqr.o365.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class TaUser {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int seqNo;
+	private String userId;
+	private String passwd;
+	public int getSeqNo() {
+		return seqNo;
+	}
+	public void setSeqNo(int seqNo) {
+		this.seqNo = seqNo;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+}
---
+++ src/main/resources/ta_master_cd.sql
@@ -0,0 +1,3 @@
+
+insert into ta_master_cd(key_ty,cd,decode,start_dt,end_dt,create_dt,last_update_id,last_update_dt) values ('GLOBAL_REG','Y','turn on the function to register the admin user',sysdate,null,sysdate,'o365',sysdate);
+commit;
