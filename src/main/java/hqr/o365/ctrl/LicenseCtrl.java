3edb9ddddbbb75931baf613d33ce8ebc3a882d43
Van You See <448261751@qq.com>
2021-05-07 16:50:55 +0800

    add new tabs

---
+++ src/main/java/hqr/o365/ctrl/LicenseCtrl.java
@@ -0,0 +1,13 @@
+package hqr.o365.ctrl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LicenseCtrl {
+	
+	@RequestMapping(value = {"/tabs/license.html"})
+	public String dummy() {
+		return "tabs/license";
+	}
+}
