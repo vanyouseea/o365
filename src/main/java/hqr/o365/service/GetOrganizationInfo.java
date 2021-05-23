7b902148edc14e996f1a3df3b2535178934755b6
Van You See <448261751@qq.com>
2021-05-23 17:29:02 +0800

    When create user, allow to custom the location or follow as organization's location

---
+++ src/main/java/hqr/o365/service/GetOrganizationInfo.java
@@ -0,0 +1,77 @@
+package hqr.o365.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import hqr.o365.dao.TaMasterCdRepo;
+import hqr.o365.domain.TaMasterCd;
+
+/**
+ * Get the organization default location
+ * When creating user use organization location, if not found or invalid, then use US location.
+ */
+
+@Service
+public class GetOrganizationInfo {
+	
+	private RestTemplate restTemplate = new RestTemplate();
+	
+	@Autowired
+	private TaMasterCdRepo tmc;
+	
+	@Value("${UA}")
+    private String ua;
+	
+	public String getUsageLocation(String accessToken) {
+		String location = "US";
+		
+		Optional<TaMasterCd> opt = tmc.findById("CUSTOM_USAGE_LOCATION_IND");
+		if(opt.isPresent()) {
+			TaMasterCd config = opt.get();
+			if("Y".equals(config.getCd())){
+				//get custom location from db
+				Optional<TaMasterCd> opt2 = tmc.findById("DEFAULT_USAGE_LOCATION");
+				if(opt2.isPresent()) {
+					TaMasterCd config2 = opt2.get();
+					location = config2.getCd();
+				}
+			}
+			else {
+				//use organization's location
+				String endpoint = "https://graph.microsoft.com/v1.0/organization?$select=countryLetterCode";
+				HttpHeaders headers = new HttpHeaders();
+				headers.set(HttpHeaders.USER_AGENT, ua);
+				headers.add("Authorization", "Bearer "+accessToken);
+				String body="";
+				HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
+				try {
+					ResponseEntity<String> response= restTemplate.exchange(endpoint, HttpMethod.GET, requestEntity, String.class);
+					JSONObject jo = JSON.parseObject(response.getBody());
+					JSONArray ja = jo.getJSONArray("value");
+					//get 1st one
+					JSONObject jb = (JSONObject)ja.get(0);
+					location = jb.getString("countryLetterCode");
+				}
+				catch (Exception e) {
+					System.out.println("Fail to get usagelocation, use US as default");
+				}
+			}
+		}
+		System.out.println("Final location is "+location);
+		return location;
+	}
+	
+}
--- src/main/resources/ta_master_cd.sql
+++ src/main/resources/ta_master_cd.sql
@@ -1,5 +1,5 @@
 --sys use prop
-insert into ta_master_cd(key_ty,cd,decode,start_dt,end_dt,create_dt,last_update_id,last_update_dt) values ('GLOBAL_REG','Y','turn on the function to register the admin user',sysdate,null,sysdate,'o365',sysdate);
+insert into ta_master_cd(key_ty,cd,decode,start_dt,end_dt,create_dt,last_update_id,last_update_dt) values ('GLOBAL_REG','Y','Y to turn on the function to register the admin user',sysdate,null,sysdate,'o365',sysdate);
 insert into ta_master_cd(key_ty,cd,decode,start_dt,end_dt,create_dt,last_update_id,last_update_dt) values ('DEFAULT_PASSWORD','Mjj@1234','the default password for the new created office user',sysdate,null,sysdate,'o365',sysdate);
 insert into ta_master_cd(key_ty,cd,decode,start_dt,end_dt,create_dt,last_update_id,last_update_dt) values ('DEFAULT_ADMIN_ROLE_ID','62e90394-69f5-4237-9190-012177145e10','the default admin role to grant or revoke',sysdate,null,sysdate,'o365',sysdate);
 
@@ -7,6 +7,9 @@ insert into ta_master_cd(key_ty,cd,decode,start_dt,end_dt,create_dt,last_update_
 insert into ta_master_cd(key_ty,cd,decode,start_dt,end_dt,create_dt,last_update_id,last_update_dt) values ('SEARCH_ROLE_2','fe930be7-5e62-47db-91af-98c3a49a38b1','User Administrator',sysdate,null,sysdate,'o365',sysdate);
 insert into ta_master_cd(key_ty,cd,decode,start_dt,end_dt,create_dt,last_update_id,last_update_dt) values ('SEARCH_ROLE_3','e8611ab8-c189-46e8-94e1-60213ab1f814','Privileged Role Administrator',sysdate,null,sysdate,'o365',sysdate);
 insert into ta_master_cd(key_ty,cd,decode,start_dt,end_dt,create_dt,last_update_id,last_update_dt) values ('SEARCH_ROLE_4','7be44c8a-adaf-4e2a-84d6-ab2649e08a13','Privileged Authentication Administrator',sysdate,null,sysdate,'o365',sysdate);
+
+insert into ta_master_cd(key_ty,cd,decode,start_dt,end_dt,create_dt,last_update_id,last_update_dt) values ('CUSTOM_USAGE_LOCATION_IND','N','Y to turn on the custom location function, if N, new office user usagelocation is same as organization',sysdate,null,sysdate,'o365',sysdate);
+insert into ta_master_cd(key_ty,cd,decode,start_dt,end_dt,create_dt,last_update_id,last_update_dt) values ('DEFAULT_USAGE_LOCATION','US','the default location for the new created office user, only valid when CUSTOM_USAGE_LOCATION_IND=Y',sysdate,null,sysdate,'o365',sysdate);
 
 
 commit;
