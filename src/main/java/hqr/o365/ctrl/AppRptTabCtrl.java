64acf3fe07040f3f1f3951d08e55b89486a4bea4
He Rui Long <ruilong@ncsi.com.cn>
2021-09-13 17:34:37 +0800

    add feature

--- src/main/java/hqr/o365/ctrl/AppRptTabCtrl.java
+++ src/main/java/hqr/o365/ctrl/AppRptTabCtrl.java
@@ -1,11 +1,20 @@
 package hqr.o365.ctrl;
 
+import java.io.File;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import hqr.o365.service.GetAppRptService;
+import hqr.o365.service.GetExchangeRpt;
+import hqr.o365.service.GetOnedriveRpt;
 import hqr.o365.service.ScanAppStatusService;
 
 @Controller
@@ -17,6 +26,11 @@ public class AppRptTabCtrl {
 	@Autowired
 	private ScanAppStatusService sass;
 	
+	@Autowired
+	private GetExchangeRpt ger;
+	
+	@Autowired
+	private GetOnedriveRpt gor;
 	
 	@RequestMapping(value = {"/tabs/apprpt.html"})
 	public String dummy() {
@@ -42,6 +56,32 @@ public class AppRptTabCtrl {
 		}
 		
 		return gars.getSysRpt(intRows, intPage);
+	}
+	
+	@ResponseBody
+	@RequestMapping(value= {"/getExchangeRpt"})
+	public ResponseEntity<FileSystemResource> getExchangeRpt(int seqNo){
+		ger.execute(seqNo);
+		return export(new File("exchange_details.csv"));
+	}
+	
+	@ResponseBody
+	@RequestMapping(value= {"/getOnedriveRpt"})
+	public ResponseEntity<FileSystemResource> getOnedriveRpt(int seqNo){
+		gor.execute(seqNo);
+		return export(new File("onedrive_details.csv"));
+	}
+	
+	public ResponseEntity<FileSystemResource> export(File file) { 
+		HttpHeaders headers = new HttpHeaders();
+	    headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+	    headers.add("Content-Disposition", "attachment; filename="+file.getName());
+	    headers.add("Pragma", "no-cache");
+	    headers.add("Expires", "0");
+	    headers.add("Last-Modified", new Date().toString());
+	    headers.add("ETag", String.valueOf(System.currentTimeMillis()));
+	 
+	    return ResponseEntity.ok().headers(headers) .contentLength(file.length()) .contentType(MediaType.parseMediaType("text/csv")) .body(new FileSystemResource(file));
 	}
 	
 	@ResponseBody
---
+++ src/main/java/hqr/o365/service/GetExchangeRpt.java
@@ -0,0 +1,65 @@
+package hqr.o365.service;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import hqr.o365.dao.TaAppRptRepo;
+import hqr.o365.domain.TaAppRpt;
+
+@Service
+public class GetExchangeRpt {
+	
+	private RestTemplate restTemplate = new RestTemplate();
+	
+	@Autowired
+	private TaAppRptRepo tarr;
+	
+	@Autowired
+	private ValidateAppInfo vai;
+	
+	@Value("${UA}")
+    private String ua;
+	
+	public boolean execute(int seqNo) {
+		boolean flag = false;
+		Optional<TaAppRpt> opt = tarr.findById(seqNo);
+		if(opt.isPresent()) {
+			TaAppRpt taAppRpt = opt.get();
+			String accessToken = "";
+			if(vai.checkAndGet(taAppRpt.getTenantId(), taAppRpt.getAppId(), taAppRpt.getSecretId())) {
+				accessToken = vai.getAccessToken();
+			}
+			if(!"".equals(accessToken)) {
+				String endpoint = "https://graph.microsoft.com/v1.0/reports/getEmailActivityUserDetail(period='D30')";
+				HttpHeaders headers = new HttpHeaders();
+				headers.set(HttpHeaders.USER_AGENT, ua);
+				headers.add("Authorization", "Bearer "+accessToken);
+				String body="";
+				HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
+				try(BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("exchange_details.csv"), "GB2312"))) {
+					ResponseEntity<String> response= restTemplate.exchange(endpoint, HttpMethod.GET, requestEntity, String.class);
+					if(response.getStatusCodeValue()==200) {
+						String text = response.getBody();
+						bw.write(text);
+						bw.flush();
+						flag = true;
+					}
+				}
+				catch (Exception e) {
+				}
+			}
+		}
+		return flag;
+	}
+}
